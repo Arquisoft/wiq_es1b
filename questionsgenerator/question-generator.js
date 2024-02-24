308e67f1fcb194897fab4501334ac250f827db4a
uo289029 <uo289029@uniovi.es>
2024-02-24 16:45:14 +0100

    Done the choosing one correct answer and three incorrect answers

--- questionsgenerator/question-generator.js
+++ questionsgenerator/question-generator.js
@@ -1,19 +1,3 @@
-
-
-var 
-	sparqlQuery = "SELECT ?country ?countryLabel ?capital ?capitalLabel\n" +
-        "WHERE {\n" +
-        "  ?country wdt:P31 wd:Q3624078;         # Q3624078 es la clase para países\n" +
-        "           wdt:P36 ?capital.            # P36 es la propiedad para la capital de un país\n" +
-        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
-        "}";
-
-makeSPARQLQuery( endpointUrl, sparqlQuery, function( data ) {
-		$( 'body' ).append( $( '<pre>' ).text( JSON.stringify( data ) ) );
-		console.log( data );
-	}
-);
-
 /**
  * template json
  * guardar: pregunta en español sin dato especifico (completar al hacer la pregunta), 
@@ -40,7 +24,7 @@ class questionGenerator{
       headers: { Accept: 'application/sparql-results+json' },
       data: { query: sparqlQuery }
     };
-    return $.ajax( this.endpointUrl, settings ).then( doneCallback );
+    return $.ajax( this.endpointUrl, settings );
   }
 
 }
--- questionsgenerator/questions-service.js
+++ questionsgenerator/questions-service.js
@@ -17,12 +17,45 @@ const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
 mongoose.connect(mongoUri);
 
 
+var sparqlQuery = "SELECT ?country ?countryLabel ?capital ?capitalLabel\n" +
+        "WHERE {\n" +
+        "  ?country wdt:P31 wd:Q3624078;         # Q3624078 es la clase para países\n" +
+        "           wdt:P36 ?capital.            # P36 es la propiedad para la capital de un país\n" +
+        "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
+        "}";
+
 //An instance of the question generator
 const generator = new QuestionGenerator();
 
+//We get que question and save the data, the results of que sparql query
+var questionData;
+var numberOfCorrectAnswer;
+var incorrectAnswers = new Set();
+generator.makeSPARQLQuery(sparqlQuery)
+          //wait for the answer from wikidata
+          .then(function(data) {
+            questionData = data.results.bindings;
 
+            //number of options of responses, the length of the array from wikidata
+            var numOptions = questionData.length
 
+            //randomly choose the correct answer
+            numberOfCorrectAnswer = Math.floor(Math.random() * numOptions);
 
+            //choose the 3 incorrect answers randomly
+            for (let i = 0; i < 3; i++) {
+
+                var numberChosen = Math.floor(Math.random() * numOptions);  
+                //check que number selected is not the same as the correct answer
+                //or has already been chose for other wrong answer
+                while (numberChosen === numberOfCorrectAnswer || incorrectAnswers.has(numberChosen)) {
+                    numberChosen = Math.floor(Math.random() * numOptions);
+                }
+                incorrectAnswers.add(numberChosen);
+            }
+
+          }
+);
 
 const server = app.listen(port, () => {
   console.log(`Question Generator Service listening at http://localhost:${port}`);
