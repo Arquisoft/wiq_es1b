74a4c6edef54b3b0b4979ffa1639c9e057ca3914
uo289029 <uo289029@uniovi.es>
2024-02-27 20:58:23 +0100

    adapting queries and getting data querys

--- questionsgenerator/questions-service.js
+++ questionsgenerator/questions-service.js
@@ -1,7 +1,6 @@
 // questions-service.js
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const QuestionGenerator = require('question-generator.js');
 const Question = require('./question-model')
@@ -13,35 +12,37 @@ const port = 8003;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/userdb';
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/questiondb';
 mongoose.connect(mongoUri);
 
 //TODO: TAKE FROM THE DATABASE ONE RANDOM QUERY OR DEPENDING OF THE THEME OF THE GAME TO MAKE THE QUESTION
 //in the database has to be the question in spanish and the sparql query
 
-// Find the user by username in the database
-const queryTitle = await Question.findOne({ username });
-const entradaAleatoria = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
+// Find a question in the database
+const randomQuestion = await Question.aggregate([{ $sample: { size: 1 } }]);
 
-
+if (randomQuestion.length > 0) {
+  //Get que information of the random question
+  const { title, query } = randomQuestion[0];
+}
 //THEME: countries
 
 //question to get the countries and their capital, fast to execute
 var questionTitle = "¿Cual es la capital de ? ?" //sustitute que ? with the country name
-var sparqlQuery = "SELECT ?country ?countryLabel ?capital ?capitalLabel\n" +
+var sparqlQuery = "SELECT ?entity ?entityLabel ?answer ?answerLabel\n" +
         "WHERE {\n" +
-        "  ?country wdt:P31 wd:Q3624078;         # Q3624078 es la clase para países\n" +
-        "           wdt:P36 ?capital.            # P36 es la propiedad para la capital de un país\n" +
+        "  ?entity wdt:P31 wd:Q3624078;         # Q3624078 es la clase para países\n" +
+        "           wdt:P36 ?answer.            # P36 es la propiedad para la capital de un país\n" +
         "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
         "}";
 
 //get all the films and director of a specific year (2020), takes longer to execute
 var questionTitle2 = "¿Quien fue el director de ? ?" //sustitute que ? with the directors name
-var otherSparqlQuery = "SELECT ?pelicula ?peliculaLabel ?director ?directorLabel\n" +
+var otherSparqlQuery = "SELECT ?entity ?entityLabel ?answer ?answerLabel\n" +
         "WHERE {\n" +
-        "  ?pelicula wdt:P31/wdt:P279* wd:Q11424 ;  # Selecciona instancias de películas\n" +
+        "  ?entity wdt:P31/wdt:P279* wd:Q11424 ;  # Selecciona instancias de películas\n" +
         "           wdt:P577 ?fecha .                # Obtiene la fecha de publicación\n" +
-        "  ?pelicula wdt:P57 ?director .             # Obtiene el director de la película\n" +
+        "  ?entity wdt:P57 ?answer .             # Obtiene el director de la película\n" +
         "  FILTER(YEAR(?fecha) = 2020)\n" +
         "  \n" +
         "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
@@ -49,32 +50,35 @@ var otherSparqlQuery = "SELECT ?pelicula ?peliculaLabel ?director ?directorLabel
 
 //fast
 var questionTitle3 = "¿Cuánta es la poblacion de ? ?";
-var sparqlQuery3 = "SELECT ?country ?countryLabel ?population\n" +
+var sparqlQuery3 = "SELECT ?entity ?entityLabel ?answerLabel\n" +
 "WHERE {\n" +
-"  ?country wdt:P31 wd:Q3624078;          # Clase para países\n" +
-"           wdt:P1082 ?population.       # Propiedad para la población\n" +
+" ?entity wdt:P31 wd:Q3624078;          # Clase para países\\n\" +\n" +
+"           wdt:P1082 ?answerLabel.       # Propiedad para la población\\n\" +\n" +
 "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
 "}";
+
 var questionTitle4 = "¿Quien es el presidente de ? ?";
-var sparqlQuery4 = "SELECT ?country ?countryLabel ?president ?presidentLabel\n" +
+var sparqlQuery4 = "SELECT ?entity ?entityLabel ?answer ?answerLabel\n" +
 "WHERE {\n" +
-"  ?country wdt:P31 wd:Q3624078;          # Clase para países\n" +
-"           wdt:P35 ?president.          # Propiedad para el presidente\n" +
+"  ?entity wdt:P31 wd:Q3624078;          # Clase para países\\n\" +\n" +
+"           wdt:P35 ?answer.          # Propiedad para el presidente\\n\" +\n" +
 "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
 "}";
+
 var questionTitle5 = "¿Cual es la moneda de ? ?";
-var sparqlQuery5 = "SELECT ?country ?countryLabel ?currency ?currencyLabel\n" +
+var sparqlQuery5 = "SELECT ?entity ?entityLabel ?answer ?answerLabel\n" +
 "WHERE {\n" +
-"  ?country wdt:P31 wd:Q3624078;          # Clase para países\n" +
-"           wdt:P38 ?currency.           # Propiedad para la moneda\n" +
+"  ?entity wdt:P31 wd:Q3624078;          # Clase para países\\n\" +\n" +
+"           wdt:P38 ?answer.           # Propiedad para la moneda\\n\" +\n" +
 "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
 "}";
+
 //the flag label is the url of the flag, to show it
 var questionTitle6 = "¿Cual es la bandera de ? ?";
-var sparqlQuery6 = "SELECT ?country ?countryLabel ?flag ?flagLabel\n" +
+var sparqlQuery6 = "SELECT ?entity ?entityLabel ?answer ?answerLabel\n" +
 "WHERE {\n" +
-"  ?country wdt:P31 wd:Q6256;             # Clase para países\n" +
-"           wdt:P41 ?flag.                # Propiedad para la bandera\n" +
+"  ?entity wdt:P31 wd:Q6256;             # Clase para países\\n\" +\n" +
+"           wdt:P41 ?answer.                # Propiedad para la bandera\\n\" +\n" +
 "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
 "}";
 
@@ -90,6 +94,13 @@ const generator = new QuestionGenerator();
 var questionData;
 var numberOfCorrectAnswer;
 var incorrectAnswers = new Set();
+
+//data of the answers
+var correctLabel;
+var correctAnswerLabel;
+
+var incorrectAnswersLabels = new Set();
+
 generator.makeSPARQLQuery(sparqlQuery)
           //wait for the answer from wikidata
           .then(function(data) {
@@ -100,6 +111,8 @@ generator.makeSPARQLQuery(sparqlQuery)
 
             //randomly choose the correct answer
             numberOfCorrectAnswer = Math.floor(Math.random() * numOptions);
+            correctLabel = questionData[numberOfCorrectAnswer].labelTitle.value;
+            correctAnswerLabel = questionData[numberOfCorrectAnswer].label.value;
 
             //choose the 3 incorrect answers randomly
             for (let i = 0; i < 3; i++) {
@@ -110,6 +123,7 @@ generator.makeSPARQLQuery(sparqlQuery)
                 while (numberChosen === numberOfCorrectAnswer || incorrectAnswers.has(numberChosen)) {
                     numberChosen = Math.floor(Math.random() * numOptions);
                 }
+                incorrectAnswersLabels.add(questionData[numberChosen].label.value);
                 incorrectAnswers.add(numberChosen);
             }
 
