61a5a234135695639e7c816cb76859670caeb154
uo289769 <uo289769@uniovi.es>
2024-03-01 17:21:11 +0100

    Adding new class Home.js

---
+++ webapp/src/components/Home.js
@@ -0,0 +1,70 @@
+// src/components/Home.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
+
+const Home = () => {
+  const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+
+  const loginUser = async () => {
+    try {
+      const response = await axios.post(`${apiEndpoint}/home`, { username, password });
+
+      // Extract data from the response
+      const { createdAt: userCreatedAt } = response.data;
+
+      setOpenSnackbar(true);
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
+      {loginSuccess ? (
+        <div>
+          <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+            Hello {username}!
+          </Typography>
+          <Typography component="p" variant="body1" sx={{ textAlign: 'center', marginTop: 2 }}>
+            Your account was created on {new Date(createdAt).toLocaleDateString()}.
+          </Typography>
+        </div>
+      ) : (
+        <div>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button variant="contained" color="primary" onClick={loginUser}>
+            Login
+          </Button>
+          <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar} message="Login successful" />
+          {error && (
+            <Snackbar open={!!error} autoHideDuration={6000} onClose={() => setError('')} message={`Error: ${error}`} />
+          )}
+        </div>
+      )}
+    </Container>
+  );
+};
+
+export default Home;
