b8d9f7b48a032809f2bba1b6e981f2b553499f22
uo289029 <uo289029@uniovi.es>
2024-03-28 12:51:04 +0100

    finished login tests

--- webapp/e2e/features/login-form.feature
+++ webapp/e2e/features/login-form.feature
@@ -4,3 +4,8 @@ Scenario: The user is registered in the site
   Given A registered user
   When I fill the data in the form and press submit
   Then The Home is shown in the screen
+
+Scenario: The user is registered in the site, uses wrong password
+  Given A registered user
+  When I fill the data in the form with invalid password and press submit
+  Then A error message should be shown in the screen
--- webapp/e2e/steps/login-form.steps.js
+++ webapp/e2e/steps/login-form.steps.js
@@ -19,7 +19,10 @@ defineFeature(feature, test => {
     //Way of setting up the timeout
     setDefaultOptions({ timeout: 10000 })
 
+  });
 
+  //before each test, the page is back to the login page
+  beforeEach(async () => {
     await page
       .goto("http://localhost:3000", {
         waitUntil: "networkidle0",
@@ -44,7 +47,28 @@ defineFeature(feature, test => {
     });
 
     then('The Home is shown in the screen', async () => {
-        await expect(page).toMatchElement("div", { text: "Home" });
+        await expect(page).toMatchElement("div", { text: "Hello "+username+"!" });
+    });
+  })
+
+  test('The user is registered in the site, uses wrong password', ({given,when,then}) => {
+    
+    let username;
+    let password;
+
+    given('A registered user', async () => {
+      username = "userrr"
+      password = "MaMaMM3454*/============="
+    });
+
+    when('I fill the data in the form with invalid password and press submit', async () => {
+      await expect(page).toFill('input[name="username"]', username);
+      await expect(page).toFill('input[name="password"]', password);
+      await expect(page).toClick('button', { text: 'Login' })
+    });
+
+    then('A error message should be shown in the screen', async () => {
+        await expect(page).toMatchElement("div", { text: "Invalid credentials" });
     });
   })
 
