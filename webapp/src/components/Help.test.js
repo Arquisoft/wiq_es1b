aeb97d6c8b7258593f05775d4c7926a238c411f3
Mauro Varea Fernández <uo270734@uniovi.es>
2024-04-06 20:03:04 +0200

    Second try of Help test

--- webapp/src/components/Help.test.js
+++ webapp/src/components/Help.test.js
@@ -10,6 +10,12 @@ const mockAxios = new MockAdapter(axios);
 const titleId = 'title';
 const paragraph1 = 'WIQ is a game that tests your knowledge with questions featuring multiple possible answers, only one of which is correct, based on information extracted directly from WikiData.';
 
+const subtitle1 = 'How to play';
+const ol1 = '1. Start a game from the Home section.';
+const ol2 = '2. Select the answer that you believe is the correct.';
+const ol3 = '3. Verify whether your answer was correct or not.';
+const ol4 = "4. Proceed to the next question when you're ready.";
+const ol5 = '5. Repeat until reaching the end.';
 
 describe('AboutUs component', () => {
   beforeEach(() => {
@@ -26,6 +32,13 @@ describe('AboutUs component', () => {
 
     expect(screen.getByTestId(titleId)).toBeInTheDocument();
     expect(screen.getByText(paragraph1)).toBeInTheDocument();
+
+    expect(screen.getByText(subtitle1)).toBeInTheDocument();
+    expect(screen.getByText(ol1)).toBeInTheDocument();
+    expect(screen.getByText(ol2)).toBeInTheDocument();
+    expect(screen.getByText(ol3)).toBeInTheDocument();
+    expect(screen.getByText(ol4)).toBeInTheDocument();
+    expect(screen.getByText(ol5)).toBeInTheDocument();
 
   });
 
