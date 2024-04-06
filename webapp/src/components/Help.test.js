d75be333ee9bd04160beed40eb1dec7710cb7592
Mauro Varea Fernández <uo270734@uniovi.es>
2024-04-06 14:26:08 +0200

    Third try of Help test

--- webapp/src/components/Help.test.js
+++ webapp/src/components/Help.test.js
@@ -17,6 +17,16 @@ const ol3 = '3. Verify whether your answer was correct or not.';
 const ol4 = "4. Proceed to the next question when you're ready.";
 const ol5 = '5. Repeat until reaching the end.';
 
+const subtitle2 = 'Website sections';
+const ul1title = 'Home';
+const ul1text = 'The Home section is where you are welcomed and can start a game.';
+const ul2title = 'Record';
+const ul2text = 'The Record section is where you can view your played games and their results.';
+const ul3title = 'Help';
+const ul3text = 'The Help section is where you can learn how to navigate through our website and play the WIQ game. You are here!';
+const ul4title = 'About us';
+const ul4text = 'The About us section is where you can discover information about the development team behind the WIQ game.';
+
 describe('AboutUs component', () => {
   beforeEach(() => {
     mockAxios.reset();
@@ -40,6 +50,15 @@ describe('AboutUs component', () => {
     expect(screen.getByText(ol4)).toBeInTheDocument();
     expect(screen.getByText(ol5)).toBeInTheDocument();
 
+    expect(screen.getByText(subtitle2)).toBeInTheDocument();
+    expect(screen.getByText(ul1title)).toBeInTheDocument();
+    expect(screen.getByText(ul1text)).toBeInTheDocument();
+    expect(screen.getByText(ul2title)).toBeInTheDocument();
+    expect(screen.getByText(ul2text)).toBeInTheDocument();
+    expect(screen.getAllByText(ul3title)[1]).toBeInTheDocument();
+    expect(screen.getByText(ul3text)).toBeInTheDocument();
+    expect(screen.getByText(ul4title)).toBeInTheDocument();
+    expect(screen.getByText(ul4text)).toBeInTheDocument();
   });
 
 });
