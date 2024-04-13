4a943b48fc84cd29fb5a3bcb196f03c768536bd3
uo289029 <uo289029@uniovi.es>
2024-04-13 13:32:27 +0200

    the user can now choose an exact value between 1 and 20 for the configuration by an input

--- webapp/src/components/SettingsPage.js
+++ webapp/src/components/SettingsPage.js
@@ -1,6 +1,6 @@
 // src/components/AboutUs.js
 import React, { useEffect, useState } from 'react';
-import { Container, Button, Tooltip, Menu, MenuItem, Fade, Typography} from '@mui/material';
+import { Container, Typography, TextField} from '@mui/material';
 import { useNavigate } from "react-router-dom";
 
 const SettingsPage = () => {
@@ -14,122 +14,75 @@ const SettingsPage = () => {
     // eslint-disable-next-line
   }, []);
 
-  //to manage the timer
-  const [anchorElT, setAnchorElT] = useState(null);
-  const [selectedTimer, setSelectedTimer] = useState(() => {
-    const savedTimer = localStorage.getItem('selectedTimer');
-    return savedTimer ? Number(savedTimer) : 15;
-  });
-  const openT = Boolean(anchorElT);
-  const handleClickT = (event) => {
-    setAnchorElT(event.currentTarget);
-  };
-  const handleCloseT = () => {
-    setAnchorElT(null);
-  };
-  const handleMenuItemTimeClick = (time) => {
-    setSelectedTimer(time);
-    localStorage.setItem('selectedTimer', time);
-    handleCloseT();
-  };
-
-  //to manage the number of questions
-  const [anchorEl, setAnchorEl] = useState(null);
+  //manage the number of questions
   const [selectedNumQuestions, setSelectedNumQuestions] = useState(() => {
     const savedNumQuestions = localStorage.getItem('selectedNumQuestions');
     return savedNumQuestions ? Number(savedNumQuestions) : 10;
   });
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleNumQuestionsChange = (event) => {
+    // to limit the number that the user inputs 
+    let value = event.target.value;
+    if (value < 1) {
+      value = 1;
+    } else if (value > 20) {
+      value = 20;
+    }
+    setSelectedNumQuestions(value);
+    localStorage.setItem('selectedNumQuestions', value);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleTimerChange = (event) => {
+    // to limit the number that the user inputs 
+    let value = event.target.value;
+    if (value < 1) {
+      value = 1;
+    } else if (value > 20) {
+      value = 20;
+    }
+    setSelectedTimer(value);
+    localStorage.setItem('selectedTimer', value);
   };
-  const handleMenuItemQuestionClick = (numQuestions) => {
-    setSelectedNumQuestions(numQuestions);
-    localStorage.setItem('selectedNumQuestions', numQuestions);
-    handleClose();
-  };
+
+  //to manage the timer
+  const [selectedTimer, setSelectedTimer] = useState(() => {
+    const savedTimer = localStorage.getItem('selectedTimer');
+    return savedTimer ? Number(savedTimer) : 15;
+  });
+  
 
   return(
     <Container component="main" maxWidth="md" sx={{ margin: 8 }}>
-      <Typography variant="h4" component="h1" sx={{ paddingBottom: '2em' }}>
+      <Typography variant="h4" component="h1" sx={{ paddingBottom: '2em', textAlign: 'center' }}>
         Configuration
       </Typography>
-      <div>
-          <div style={{ marginTop: '0.5em', marginBottom: '0.5em' }}>
-          <Tooltip title="button to change the number of questions of the game">
-            <Button
-              id="fade-button"
-              aria-controls={open ? 'fade-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-              style={{ backgroundColor: "white", color: 'black', width: '27em' }}
-            >
-              Press to change nº of questions: {selectedNumQuestions}.
-            </Button>
-          </Tooltip>
-          <Menu
-            id="fade-menu"
-            MenuListProps={{
-              'aria-labelledby': 'fade-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            TransitionComponent={Fade}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-          >
-            <MenuItem onClick={() => handleMenuItemQuestionClick(5)}>5</MenuItem>
-            <MenuItem onClick={() => handleMenuItemQuestionClick(10)}>10</MenuItem>
-            <MenuItem onClick={() => handleMenuItemQuestionClick(15)}>15</MenuItem>
-          </Menu>
-        </div>
-        <div >
-          <Tooltip title="button to change the time limit for each question of the game">
-            <Button
-              id="fade-button"
-              aria-controls={openT ? 'fade-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={openT ? 'true' : undefined}
-              onClick={handleClickT}
-              style={{ backgroundColor: "white", color: 'black', width: '27em' }}
-            >
-              Press to change the time limit: {selectedTimer}.
-            </Button>
-          </Tooltip>
-          <Menu
-            id="fade-menu"
-            MenuListProps={{
-              'aria-labelledby': 'fade-button',
-            }}
-            anchorEl={anchorElT}
-            open={openT}
-            onClose={handleCloseT}
-            TransitionComponent={Fade}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-          >
-            <MenuItem onClick={() => handleMenuItemTimeClick(10)}>10</MenuItem>
-            <MenuItem onClick={() => handleMenuItemTimeClick(15)}>15</MenuItem>
-            <MenuItem onClick={() => handleMenuItemTimeClick(20)}>20</MenuItem>
-          </Menu>
-        </div>
+      <div style={{ 
+        display: 'grid', 
+        gridTemplateColumns: 'repeat(2, 1fr)', 
+        gridTemplateRows: 'repeat(2, 1fr)', 
+        gridColumnGap: '15px', 
+        gridRowGap: '15px' 
+      }}> 
+        <Typography variant="h6" component="h2">
+          Number of questions:
+        </Typography>
+        <TextField
+          type="number"
+          inputProps={{ min: 1, max: 20 }}
+          value={selectedNumQuestions}
+          onChange={handleNumQuestionsChange}
+          label="questions"
+          variant="outlined"
+        />
+        <Typography variant="h6" component="h2">
+          Time limit:
+        </Typography>
+        <TextField
+          type="number"
+          inputProps={{ min: 1, max: 20 }}
+          value={selectedTimer}
+          onChange={handleTimerChange}
+          label="time"
+          variant="outlined"
+        />
       </div>
     </Container>
   );
