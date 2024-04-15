d0966fd2e8deefd2cd822387213cd70027cd28f8
Mauro Varea Fernández <uo270734@uniovi.es>
2024-04-15 13:39:53 +0200

    Update AboutUs.js

--- webapp/src/components/AboutUs.js
+++ webapp/src/components/AboutUs.js
@@ -1,7 +1,7 @@
 // src/components/AboutUs.js
 import React, { useEffect } from 'react';
-import { Container, Typography, List, ListItem, ListItemText } from '@mui/material';
 import { useNavigate } from "react-router-dom";
+import { Container, Typography, List, ListItem, ListItemText } from '@mui/material';
 
 const AboutUs = () => {
   const navigate = useNavigate();
