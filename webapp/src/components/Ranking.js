02f34c022b1ec9c64f19c4123e8bce865e810ed8
uo289769 <uo289769@uniovi.es>
2024-04-16 16:06:56 +0200

    Method to calculate the score of each user

--- webapp/src/components/Ranking.js
+++ webapp/src/components/Ranking.js
@@ -16,8 +16,40 @@ const Ranking = () => {
       }
       // eslint-disable-next-line 
     }, []);
+
+    //accedo al usuario logeado
+    const location = useLocation();
+    const { username } = location.state || {};
+    const { createdAt } = location.state || {};
+
+    const [record, setRecord] = useState([]);
+    const [ranking, setRanking] = useState([]);
+    const [puntuacion, setPuntuacion] = useState(0);
+
+    //data for the chart
+    const [loading, setLoading] = useState(true);
   
+    const getHistorialForLoggedUser = async () => {
+        const response = await axios.post(`${apiEndpoint}/getGameRecord`, { username });
+        // Extract data from the response
+        let { games } = response.data;
+        setRecord(games);
     
+        //const totalGames = games.length;
+        // Calculate correct and incorrect answers for the chartbar
+        let correctCount = 0;
+        games.forEach((game, index) => {
+          game.questions.forEach(question => {
+            if (question.correctAnswer === question.selectedAnswer) {
+              correctCount++;
+            }
+          });
+          //labels.push(`Game ${totalGames - games.length + index + 1}`);
+          setLoading(false);
+        });
+
+        return correctCount;
+    }
   
     return (
         <Container component="main" maxWidth="sm" sx={{ marginTop: 4 }}>
