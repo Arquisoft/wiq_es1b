4a34f858ed250adf268d841d31d813ae6be05bb0
Mauro Varea Fernández <uo270734@uniovi.es>
2024-04-18 17:32:28 +0200

    NavBar redesign 1

--- webapp/src/components/NavigationBar.js
+++ webapp/src/components/NavigationBar.js
@@ -4,8 +4,9 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { AppBar, Tabs, Tab, Tooltip, Menu, MenuItem } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
-import LogoutIcon from '@mui/icons-material/Logout';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import HelpIcon from '@mui/icons-material/Help';
+import LogoutIcon from '@mui/icons-material/Logout';
 import IconWIQ from '../favicon2.ico';
 
 const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
@@ -20,7 +21,12 @@ const NavigationBar = () => {
   const { selectedNumQuestions } = location.state || {};
   const { selectedTimer } = location.state || {};
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorElSettingsMenu, setAnchorElSettingsMenu] = React.useState(null);
+  const [anchorElInfoMenu, setAnchorElInfoMenu] = React.useState(null);
+
+  const deleteTempQuestions = async () => {
+    await axios.post(`${apiEndpoint}/deleteTempQuestions`, { username });
+  };
 
   const showHome = () => {
     if (username !== undefined) {
@@ -29,16 +35,23 @@ const NavigationBar = () => {
     }
   };
 
+  const showSettings = () => {
+    if (username !== undefined) {
+      deleteTempQuestions();
+      navigate("/settings", { state: { username, createdAt } });
+    }
+  };
+
+  const showApiDoc = () => {
+    window.location.href = 'http://20.26.114.153:8000/api-doc/'; //NOSONAR
+  };
+
   const showRecord = () => {
     if (username !== undefined) {
       deleteTempQuestions();
       navigate("/record", { state: { username, createdAt } });
     }
   };
-
-  const deleteTempQuestions = async () => {
-    await axios.post(`${apiEndpoint}/deleteTempQuestions`, { username });
-  }
 
   const showHelp = () => {
     if (username !== undefined) {
@@ -54,28 +67,22 @@ const NavigationBar = () => {
     }
   };
 
-  const showApiDoc = () => {
-    window.location.href = 'http://20.26.114.153:8000/api-doc/'; //NOSONAR
-  };
-
-  const showSettings = () => {
-    if (username !== undefined) {
-      deleteTempQuestions();
-      navigate("/settings", { state: { username, createdAt } });
-    }
-  };
-
   const logOut = () => {
     localStorage.removeItem('username');
     navigate('/');
-  }
-
-  const openMenu = (event) => {
-    setAnchorEl(event.currentTarget);
   };
 
-  const closeMenu = () => {
-    setAnchorEl(null);
+  const openSettingsMenu = (event) => {
+    setAnchorElSettingsMenu(event.currentTarget);
+  };
+
+  const openInfoMenu = (event) => {
+    setAnchorElInfoMenu(event.currentTarget);
+  };
+
+  const closeMenus = () => {
+    setAnchorElSettingsMenu(null);
+    setAnchorElInfoMenu(null);
   };
 
   const tabsStyle = {
@@ -100,6 +107,9 @@ const NavigationBar = () => {
     color: 'white',
     fontWeight: 'bold',
     backgroundColor: '#1976d2',
+    '&:hover': {
+      backgroundColor: '#1976d2',
+    },
   };
 
   return (
@@ -117,25 +127,25 @@ const NavigationBar = () => {
                       <img src={IconWIQ} alt="Icono" />
                     </Tooltip>}   
               onClick={showHome} />
-          <Tab label="Help"
-            sx={tabStyle}
-            onClick={showHelp} />
-          <Tab label="About Us"
-            sx={tabStyle}
-            onClick={showAboutUs} />
           <div style={{ flex: 5 }} />
           <Tab aria-label="Settings"
             sx={tabStyle}
             icon={<Tooltip title="Settings">
                     <SettingsIcon sx={tabStyle} />
                   </Tooltip>}
-            onClick={openMenu} />
+            onClick={openSettingsMenu} />
           <Tab aria-label="Record"
             sx={tabStyle}
             icon={<Tooltip title="Personal record">
                     <AccountCircle sx={tabStyle} />
                   </Tooltip>}
             onClick={showRecord} />
+          <Tab aria-label="Info"
+            sx={tabStyle}
+            icon={<Tooltip title="Info">
+                    <HelpIcon sx={tabStyle} />
+                  </Tooltip>}
+            onClick={openInfoMenu} />
           <Tab aria-label="Log out"
             sx={tabStyle}
             icon={<Tooltip title="Log out">
@@ -144,15 +154,15 @@ const NavigationBar = () => {
             onClick={logOut} />
         </Tabs>
       </AppBar>
-      <Menu aria-label="Menu"
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={closeMenu}
+      <Menu aria-label="Settings menu"
+        anchorEl={anchorElSettingsMenu}
+        open={Boolean(anchorElSettingsMenu)}
+        onClose={closeMenus}
         MenuListProps={{
           sx: menuStyle
         }}
       >
-        <MenuItem aria-label="Game Settings"
+        <MenuItem aria-label="Game settings"
           sx={menuItemStyle}
           onClick={showSettings} >
           Game settings
@@ -161,6 +171,25 @@ const NavigationBar = () => {
           sx={menuItemStyle}
           onClick={showApiDoc} >
           API DOC
+        </MenuItem>
+      </Menu>
+      <Menu aria-label="Help menu"
+        anchorEl={anchorElInfoMenu}
+        open={Boolean(anchorElInfoMenu)}
+        onClose={closeMenus}
+        MenuListProps={{
+          sx: menuStyle
+        }}
+      >
+       <MenuItem aria-label="Help"
+          sx={menuItemStyle}
+          onClick={showHelp} >
+          Help
+        </MenuItem>
+        <MenuItem aria-label="About us"
+          sx={menuItemStyle}
+          onClick={showAboutUs} >
+          About us
         </MenuItem>
       </Menu>
     </div>
